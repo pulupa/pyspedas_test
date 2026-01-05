3163b7ed6b8a42dc0fc57af9f3ee58d9fe60a839
Marc Pulupa <pulupa@berkeley.edu>
2026-01-05 10:36:40 -0800

    wind example with 'qtnfit' data

--- example_wind.py
+++ example_wind.py
@@ -1,14 +1,22 @@
 import pyspedas.projects.wind as wind
 from pyspedas import tplot, tplot_names
 
-# example of a Type III radio burst
+# example time with a Type III radio burst
 
 trange = ['2019-04-02/15:00', '2019-04-02/16:30']
+
+# example of Wind 'qtnfit' data
+
+trange = ['2005-01-01/00:00', '2005-01-02/00:00']
 
 wind.waves(trange=trange, time_clip=True, datatype='rad2')
 wind.waves(trange=trange, time_clip=True, datatype='rad1')
 wind.waves(trange=trange, time_clip=True, datatype='tnr')
 
+wind.waves(trange=trange, time_clip=True, datatype='qtnfit')
+wind.waves(trange=trange, time_clip=True, datatype='qtnfit-filtered')
+
 print(tplot_names())
 
-tplot(['wi_l2_wav_rad?_PSD_V2_Z','wi_l2_wav_tnr_PSD_V2'])
+tplot(['wi_l2_wav_rad?_PSD_V2_Z','wi_l2_wav_tnr_PSD_V2',
+       'wi_l2_wav_qtnfit_fit_n_e','wi_l2_wav_qtnfit-filtered_fit_n_e'])
